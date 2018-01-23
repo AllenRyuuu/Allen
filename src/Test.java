2ddb6a435ad59120acfdb6ec402a2ebdeacfb409
wentao.liu <12345678>
2018-01-23 15:52:28 +0800

    类持有锁

---
+++ src/Test.java
@@ -0,0 +1,21 @@
+/**
+ * Project: CCC Web Suite
+ * <p>
+ * Created by c_liuwentao on 2017/12/19.
+ * <p>
+ * Copyright 2017 CCC Corporation Limited.
+ * All rights reserved.
+ * <p>
+ * This software is the confidential and proprietary information of
+ * CCC Company. ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with http://ccc.cccis.com/
+ */
+public class Test {
+    public static void main(String[] args) {
+        byte a = 'b';
+        char b = (char) a;
+        System.out.println(b);
+    }
+}
